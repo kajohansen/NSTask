e06df5b0e8f1893201f398bc9f2a3dac0991b307
Karl Andreas <kajohansen.1@gmail.com>
2013-08-03 10:03:28 +0200

    Initial commit with starterproject from tutorial

---
+++ TasksProject/AppDelegate.h
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.h
+//  TasksProject
+//
+//  Created by Andy on 3/23/13.
+//  Copyright (c) 2013 Ray Wenderlich. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+/** 
+ * Speaking
+ */
+@property (weak) IBOutlet NSTextField *phraseField;
+- (IBAction)speak:(id)sender;
+
+/** 
+ * Project Package
+ */
+@property (unsafe_unretained) IBOutlet NSTextView *outputText;
+@property (weak) IBOutlet NSProgressIndicator *spinner;
+@property (weak) IBOutlet NSPathControl *projectPath;
+@property (weak) IBOutlet NSPathControl *repoPath;
+@property (weak) IBOutlet NSButton *buildButton;
+@property (weak) IBOutlet NSTextField *targetName;
+- (IBAction)startTask:(id)sender;
+- (IBAction)stopTask:(id)sender;
+
+/**
+ * NSTask 
+ */
+@property (nonatomic, strong) __block NSTask *buildTask;
+@property (nonatomic) BOOL isRunning;
+@property (nonatomic, strong) NSPipe *outputPipe;
+
+
+@end
---
+++ TasksProject/AppDelegate.m
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.m
+//  TasksProject
+//
+//  Created by Andy on 3/23/13.
+//  Copyright (c) 2013 Ray Wenderlich. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+
+}
+
+- (IBAction)speak:(id)sender {
+    
+}
+
+- (IBAction)startTask:(id)sender {
+    
+}
+
+- (IBAction)stopTask:(id)sender {
+    
+}
+
+@end
